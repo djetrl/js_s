710511198f1f7eba16da0ee5cd11983f076cfb01
djetrl <82327079+djetrl@users.noreply.github.com>
2022-02-14 12:37:33 +0700

    Add files via upload

--- srp.js
+++ srp.js
@@ -1,4 +1,8 @@
-let hg = 23 ;
-let wh = 10 ;
-let s = hg*wh ;
-console.log(s);
+let s = 2000000; 
+let p = 0.1;
+let years = 5;
+let per ;
+let sum = p*s;
+console.log(sum);
+per = years * sum;
+console.log(per);

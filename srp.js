e234349747ad5a8afa48d2496951c022332dc1b0
djetrl <82327079+djetrl@users.noreply.github.com>
2022-02-21 10:42:13 +0700

    Add files via upload

--- srp.js
+++ srp.js
@@ -1,3 +1,4 @@
+
 let students = [
 	{id: 1, name:"Андрей", surname:"Артамонов"},
 {id: 2, name:"Дарья", surname:"Архипова"},
@@ -6,14 +7,54 @@ let students = [
 {id: 5, name:"Матвей", surname:"Гаврилов"}
 
 ]
-	for (let i = 0; i < students.length; i++){
+
+	function load_all() {
+		let table = document.getElementById('tbl_al')
+		for (let i = 0; i < students.length; i++){
 		let id =  students[i].id
 		let name =  students[i].name
 		let surname =  students[i].surname
-
-
-		console.log('Номер ' + id )
-		console.log('Имя ' +  name)
-		console.log('Фамилия ' +  surname)
-		console.log('---------------------------')
+	//создадим строку и 3 ячейки
+	 let tr = document.createElement('tr')
+	let td1 = document.createElement('td')
+	let td2 = document.createElement('td')
+	let td3 = document.createElement('td')
+	//запишем данные в ячейки
+	td1.textContent = id
+	td2.textContent = name
+	td3.textContent = surname
+	//вставим ячейки в строку
+	tr.appendChild(td1)
+	tr.appendChild(td2)
+	tr.appendChild(td3)
+	//вставим строку в таблицу
+	table.appendChild(tr)
+}
 	}
+
+let name = document.getElementById('name')
+function load_student(id){
+	let head = document.getElementById('zagolovok')
+	head.textContent = "Информация о студенте №" + students[id].id
+	let name = document.getElementById('name')
+	name.textContent =" "+ students[id].name
+		let surname = document.getElementById('surname')
+	surname.textContent = " "+students[id].surname
+}
+let idk = 0;
+function next() {
+	idk++;
+	if (idk > 0 ) {document.getElementById('btn_prev').disabled = false;}
+	if ( idk === students.length-1) {
+		document.getElementById("btn_next").disabled = true;
+	}
+	load_student(idk)
+}
+function prev() {
+	idk=idk -1;
+	if (idk < 5 ) {document.getElementById('btn_next').disabled = false;}
+	if (idk === 0){
+		document.getElementById('btn_prev').disabled = true;
+	}
+	load_student(idk)
+}
